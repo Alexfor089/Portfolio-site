2c80fcff54399bcdf9a88b76638c2951512b4d61
Alexfor09 <56155891+Alexfor089@users.noreply.github.com>
2020-09-09 00:15:12 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+AOS.init();
+var mySwiper = new Swiper('.swiper-container', {
+   pagination: {
+      el: '.projects-pagination',
+      bulletClass: 'projects-bulet',
+      bulletActiveClass: 'projects-bulet-active',
+      clickable: true
+   },
+});
